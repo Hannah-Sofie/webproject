75d9fc0e06f864c1ff742d481cf2af01efb1036b
maheggh <111765195+maheggh@users.noreply.github.com>
2024-02-27 16:23:00 +0100

    added a openclass.js with the openclass basic layout

--- journal-web/src/App.js
+++ journal-web/src/App.js
@@ -1,31 +1,14 @@
 import React, { useState } from "react";
-import LoginPage from "./components/LoginPage";
-import RegisterPage from "./components/RegisterPage";
+import OpenClassPage from "./components/OpenClass";
 import Header from "./components/Header"; // Make sure the path to the Header component is correct
 
 function App() {
-  const [currentPage, setCurrentPage] = useState("login");
-
-  const renderPage = () => {
-    if (currentPage === "login") {
-      return <LoginPage />;
-    } else if (currentPage === "register") {
-      return <RegisterPage />;
-    }
-    // Add additional conditions for other pages if needed
-  };
-
   return (
     <div className="App">
-      <Header /> {/* This line adds the Header component to your layout */}
-      {renderPage()}
-      {/* Example buttons to switch between login and register pages */}
-      <div>
-        <button onClick={() => setCurrentPage("login")}>Login</button>
-        <button onClick={() => setCurrentPage("register")}>Register</button>
-      </div>
+        <Header />
+        {/* Other components or content */}
+        <OpenClassPage /> {/* Here you use your imported component */}
     </div>
   );
 }
-
 export default App;
