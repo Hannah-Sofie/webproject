809c482cf0236278d9daa880c61dcbc4f2faf66b
hannah-sofie <hannah-sofie@live.no>
2024-03-05 15:24:33 +0100

    fixing dockerfiles and compose files

--- journal-backend/server.js
+++ journal-backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.APP_PORT || 5001;
 
 // Middleware
 app.use(express.json());
